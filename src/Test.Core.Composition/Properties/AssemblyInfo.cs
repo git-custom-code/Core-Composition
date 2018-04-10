d175472330b6d1dea7609f237deb45bce24f8f63
Stefan Loeffler <s.loeffler@custom-co.de>
2018-04-10 13:50:44 +0200

    added shared assembly info

--- src/Test.Core.Composition/Properties/AssemblyInfo.cs
+++ src/Test.Core.Composition/Properties/AssemblyInfo.cs
@@ -1,3 +1,9 @@
-﻿using CustomCode.Core.Composition;
+using CustomCode.Core.Composition;
+using System.Reflection;
+using System.Runtime.InteropServices;
 
 [assembly: IocVisibleAssembly]
+
+[assembly: AssemblyTitle("Test.Core.Composition")]
+[assembly: AssemblyDescription("Automated tests for the Core.Composition assembly.")]
+[assembly: Guid("930d664e-032a-4692-9c85-d18d63f9dfa2")]
